06310d981ba597b3dbe38dbd79ada43427c02caf
anushagunnam502 <anushagunnam@lewisu.edu>
2025-09-26 23:17:03 -0500

    created and added basic html and app.js file

---
+++ script.js
@@ -0,0 +1,5 @@
+// This will show inside the browser console
+console.log("Hello from script.js! This is running in the browser.");
+
+// This will update something on the page
+document.getElementById("message").innerText = "JavaScript is working!";
